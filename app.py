7c6a7bf458d6757cf3da1c70b480a7d125edbd0a
yangyin3027 <yangzn23@gmail.com>
2023-11-10 11:11:11 -0600

    add cleanup mechanism

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, jsonify, request, render_template
 
-
 import io
+import os
+import signal
+import sys
 import json
 
 import torchvision.transforms as transforms
@@ -12,6 +14,8 @@ app = Flask(__name__)
 imagenet_class_index = json.load(open('imagenet_class_index.json'))
 model = models.densenet121(weights='IMAGENET1K_V1')
 model.eval()
+
+files_to_delete = set()
 
 def transform_image(image_bytes):
     my_transforms = transforms.Compose([transforms.Resize(255),
@@ -46,6 +50,7 @@ def predict():
             # convert that to bytes
             img_path = 'static/' + file.filename
             file.save(img_path)
+            files_to_delete.add(img_path)
             with open(img_path, 'rb') as f:
                 img_bytes = f.read()
             class_id, class_name = get_prediction(image_bytes=img_bytes)
@@ -60,7 +65,18 @@ def predict():
     else:
         return render_template('index.html')
 
+def cleanup_files(signum, frame):
+    for file_path in files_to_delete:
+        try:
+            os.remove(file_path)
+        except Exception as e:
+            print(f'Error deleting files: {str(e)}')
+    
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, cleanup_files)
+
 if __name__ == '__main__':
    
-    app.run(port=3000)
+    app.run()
     
